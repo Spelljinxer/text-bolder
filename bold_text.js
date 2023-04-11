bd20efd5018e524e740181c7034274b9ae7b3d58
Spelljinxer <reiden556@gmail.com>
2023-04-11 14:54:00 +0800

    changed fonts

--- bold_text.js
+++ bold_text.js
@@ -1,15 +1,17 @@
 function boldText(event) {
-    event.preventDefault(); 
-  
-    let inputText = document.getElementById("text-input").value;
-    let words = inputText.split(" ");
-  
-    for (let i = 0; i < words.length; i++) {
-      let numToBold = Math.floor(Math.random() * Math.min(words[i].length - 2, 3)) + 1;
+  event.preventDefault(); 
+
+  let inputText = document.getElementById("text-input").value;
+  let words = inputText.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 2) {
+      let numToBold = Math.floor(Math.random() * (words[i].length - 1)) + 1;
       words[i] = "<span class='bold'>" + words[i].substring(0, numToBold) + "</span>" + words[i].substring(numToBold);
     }
-  
-    let boldText = words.join(" ");
-  
-    document.getElementById("output").innerHTML = boldText;
   }
+
+  let boldText = words.join(" ");
+
+  document.getElementById("output").innerHTML = boldText;
+}
