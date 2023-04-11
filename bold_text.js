76bba9e6e66fb00045d4f6677f357711ede502ff
Spelljinxer <reiden556@gmail.com>
2023-04-11 15:02:24 +0800

    centered

--- bold_text.js
+++ bold_text.js
@@ -5,7 +5,11 @@ function boldText(event) {
   let words = inputText.split(" ");
 
   for (let i = 0; i < words.length; i++) {
-    if (words[i].length > 2) {
+    if (words[i].length === 1) {
+      words[i] = "<span class='bold'>" + words[i] + "</span>";
+    } else if (words[i].length === 2) {
+      words[i] = "<span class='bold'>" + words[i][0] + "</span>" + words[i][1];
+    } else {
       let numToBold = Math.floor(Math.random() * (words[i].length - 1)) + 1;
       words[i] = "<span class='bold'>" + words[i].substring(0, numToBold) + "</span>" + words[i].substring(numToBold);
     }
